45b5fce4d300b0e78a9f4788b7e44ccea7b3faba
ydcun <ydcun@hotmail.com>
2019-01-22 01:26:51 +0800

    获取模块中的state

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,5 +1,5 @@
 const state = {
-
+	userName: 'baoqiang'
 }
 const mutations = {
 
--- src/views/email.vue
+++ src/views/email.vue
@@ -3,6 +3,7 @@
 		ddddddd
 		<p><button @click="handleClick">点我</button></p>
 		<p>{{ appName }}</p>
+		<p>userName:{{ userName }}</p>
 	</div>
 </template>
 
@@ -16,7 +17,11 @@ export default {
 	computed: {
 		appName () {
 			return this.$store.state.appName
+		},
+		userName () {
+			return this.$store.state.user.userName
 		}
+
 	},
 	mounted() {
 		console.log(this.$bus)
