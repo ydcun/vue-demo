a353810c0aba5977cb6a8bc81ef1e1bd79671a39
ydcun <ydcun@hotmail.com>
2019-01-22 01:07:40 +0800

    bus移动到lib下

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import bus from './bus'
+import bus from './lib'
 
 
 Vue.config.productionTip = false
