a23b738b1b67e2cf9b1bdafa466dcc7683590c5f
ydcun <ydcun@hotmail.com>
2019-01-22 00:43:25 +0800

    子组件赋值

---
+++ src/components/AShow.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<p>{{ showValue }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ashow",
+	props:{
+		showValue:{
+			type: [String, Number],
+			default: ''
+		}
+	}
+}
+</script>
--- src/views/store.vue
+++ src/views/store.vue
@@ -3,12 +3,14 @@
 		<a-input v-model="inputValue"></a-input>
 		<p>{{ inputValue }}</p>
 		<a-input @input="handleInput" :value="inputValue"/>
+		<a-show :showValue="inputValue">ShowValue:</a-show>
 	</div>
 </template>
 
 <<script>
 
 import AInput from "_c/AInput.vue"
+import AShow from '_c/AShow.vue'
 
 export default {
 	name:"store",
@@ -18,7 +20,8 @@ export default {
 		}
 	},
 	components:{
-		AInput
+		AInput,
+		AShow
 	},
 	methods: {
 		handleInput (val){
